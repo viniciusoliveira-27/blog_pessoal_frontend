2c45c515855caa6725b779ce49273b433d84fb1a
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-02-12 12:57:14 -0300

    :tada: Commit inicial

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,40 @@
+
+function Home() {
+    return (
+        <div style={{
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+        }}>
+            <div>
+                <div style={{
+                    width: "80vw",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}>
+
+                    <h2>Seja Bem Vindo!</h2>
+                    <p>Expresse aqui seus pensamentos e opiniões</p>
+                </div>
+
+                <div
+                    style={{
+                        width: '80vw',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <img
+                        src="https://i.imgur.com/VpwApCU.png"
+                        alt="Imagem da Página Home"
+                        width="400px"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
