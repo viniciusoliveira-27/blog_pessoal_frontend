f3df234a2abbe633f71c9a9f5f0ea55b6e93698d
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-02-14 11:11:27 -0300

    feat: pagina Home finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
 
 function App() {
 
   return (
   <>
+    <Navbar/>
     <Home/>  
+    <Footer/>
   </>
  
   )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,40 +1,37 @@
-
 function Home() {
     return (
-        <div style={{
-            width: "100vw",
-            display: "flex",
-            justifyContent: "center",
-        }}>
-            <div>
-                <div style={{
-                    width: "80vw",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}>
+      <>
+        <div className="flex justify-center bg-indigo-900">
+          <div className="container grid grid-cols-2 text-white">
+            <div className="flex flex-col items-center justify-center gap-4 py-4">
+                <h2 className="text-5xl max-sm:text-2xl font-bold">
+                    Seja Bem Vindo(a)!
+                </h2>
+                <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
 
-                    <h2>Seja Bem Vindo!</h2>
-                    <p>Expresse aqui seus pensamentos e opiniões</p>
+                <div className="flex justify-around gap-4">
+                    <div className="rounded border-white border-solid border-2 py-2 px-4 text-white
+                    ">
+                        Nova Postagem
+                        
+                    </div>
                 </div>
 
-                <div
-                    style={{
-                        width: '80vw',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <img
-                        src="https://i.imgur.com/VpwApCU.png"
-                        alt="Imagem da Página Home"
-                        width="400px"
-                    />
-                </div>
+
             </div>
+   
+            <div className="flex justify-center">
+              <img
+                src="https://i.imgur.com/VpwApCU.png"
+                alt="Imagem da Página Home"
+                className="w-2/3"
+              />
+            </div>
+          </div>
         </div>
-    )
-}
-
-export default Home
+      </>
+    );
+  }
+   
+  export default Home;
+   
